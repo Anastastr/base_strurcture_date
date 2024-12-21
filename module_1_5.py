2511ed17b99d0a79ac79d6fd3981897f411aa096
Anastasia <a.u.strumpe@gmail.com>
2024-12-21 16:14:50 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+tuple = ("Jul, Joulu, Weihnachten, Navidad, Noel")
+immutable_var = tuple
+print(immutable_var)
+mutable_list = ["7 октября", "7 ноября", "23 февраля", "1 мая"]
+print(mutable_list[0])
+mutable_list.remove("7 октября")
+print(mutable_list)
+mutable_list.append("4 ноября")
+print(mutable_list)
+mutable_list.copy
+print(mutable_list)
